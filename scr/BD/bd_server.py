bfc3532e808e062059faf28417e5546dc179d241
Jekeveri <valdamarxyz@gmail.com>
2024-07-31 14:10:15 +0500

    Первая версия навигации

--- scr/BD/bd_server.py
+++ scr/BD/bd_server.py
@@ -30,7 +30,37 @@ def check_user_credentials(login, password, page):
             if privileges != 1:
                 scr.BD.bd_user.insert_bd_user(id_user, login_user, password_user, privileges, page)
             else:
-                scr.navigation_apps.navigations.role_definition(privileges,page)
+                scr.navigation_apps.navigations.role_definition(privileges, page)
     else:
         scr.func.show_snack_bar(page, "Нет пользователя в базе данных")  # Нормально написать
         pass
+
+
+def select_task_data(id_user):
+    conn = psycopg2.connect(
+        dbname="test",
+        user="postgres",
+        password=123321
+    )
+    cursor = conn.cursor()
+    cursor.execute(f""" SELECT * FROM get_task_data({id_user}) """)
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    if result:
+        for record in result:
+            task_id = record[0]
+            name = record[1]
+            address = record[2]
+            phone_number = record[3]
+            email = record[4]
+            meter_number = record[5]
+            instalation_day = record[6]
+            meter_type = record[7]
+            last_reading_date = record[8]
+            reading_value = record[9]
+            date_task = record[10]
+            remark = record[11]
+            status_task = record[12]
+            scr.BD.bd_user.insert_bd_task(task_id, name, address, phone_number, email, meter_number, instalation_day,
+                                          meter_type, last_reading_date, reading_value, date_task, remark, status_task)
--- scr/BD/bd_user.py
+++ scr/BD/bd_user.py
@@ -7,8 +7,9 @@ import scr.navigation_apps.navigations
 def local_user_db():
     with sl.connect('database_client.db') as db:
         cursor = db.cursor()
-        table_task = """ Create table if not exists tasks(id Integer, client_data Text, meters_data Text,last_reading Text, 
-        date Text,status Text, remark Text) """
+        table_task = """ Create table if not exists tasks(id Integer, name Text, address Text, phone_number Text, 
+        email Text, meters_number Text, instalation_day Text, meter_type Text, 
+        last_reading_date Text, last_reading_value Text, date Text, remark Text, status Text ) """
         table_picture = """ Create table if not exists picture(id Integer, value BLOB, task_id Integer) """
         table_user = """ Create table if not exists user(id Integer, login Text, password Text, privileges integer) """
         cursor.execute(table_task)
@@ -21,7 +22,18 @@ def insert_bd_user(id_user, login, password, privileges, page):
         cursor = db.cursor()
         query = f""" Insert into user values({id_user}, {login}, {password}, {privileges}) """
         cursor.execute(query)
-    scr.navigation_apps.navigations.role_definition(privileges, page)
+    scr.BD.bd_server.select_task_data(id_user)
+
+
+def insert_bd_task(task_id, name, address, phone_number, email, meter_number, instalation_day,
+                   meter_type, last_reading_date, reading_value, date_task, remark, status_task):
+    with sl.connect('database_client.db') as db:
+        cursor = db.cursor()
+        query = f""" Insert into tasks values ({task_id}, '{name}', '{address}', '{phone_number}', '{email}', 
+            '{meter_number}', '{instalation_day}', '{meter_type}', '{last_reading_date}', 
+            '{reading_value}', '{date_task}', '{remark}', '{status_task}')"""
+        cursor.execute(query)
+    #scr.navigation_apps.navigations.role_definition(privileges, page)
 
 
 def select_user_data():
